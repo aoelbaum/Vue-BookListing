606d5573b086a0d56610c7bfa751f8a2017d4375
Ashley Oelbaum <ashley.oelbaum@macys.com>
2020-02-27 13:38:47 -0500

    loop through book list, tests pass for mod2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="booklist">
-      <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
+    <ul >
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BookList',
+  name: "BookList",
   data() {
     return {
-      title: 'All Books'
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
     };
-  },
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
